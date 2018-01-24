f18d8a5f127faa347aac768ead15528f986125c2
Slavyana Monkova <slaviana_m@mail.bg>
2018-01-24 19:54:14 +0200

    Rename classes

--- client/src/pages/Albums/AlbumBox/index.jsx
+++ client/src/pages/Albums/AlbumBox/index.jsx
@@ -5,19 +5,18 @@ import './styles.scss';
 const AlbumBox = ({ album }) => {
   return (
     <div className="col-md-3">
-      <div className="property-box">
-        <div className="property-image">
+      <div className="album-box">
+        <div className="album-image">
           {album.images ? album.images[0] : 'Awaiting Photography'}
         </div>
-        <div className="property-data">
+        <div className="album-data">
           <div className="row address-data">
-            <div className="col-md-12 main-address">{album.title}</div>
-            <div className="col-md-12 sub-address">{album.description}</div>
+            <div className="col-md-12 title">{album.title}</div>
+            <div className="col-md-12 description">{album.description}</div>
           </div>
-          <div className="row seller-data">
-            <div className="col-md-6 price">Images</div>
-            <div className="col-md-6 seller">нгфифднлифн </div>
-
+          <div className="row images-data">
+            <div className="col-md-6 images-title">Images</div>
+            <div className="col-md-6 total">нгфифднлифн </div>
             <div className="col-md-6 price-type">847849</div>
             <div className="col-md-6 seller-type">идфнхидснл</div>
           </div>
