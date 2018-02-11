36d657f02f1c007bf40387490d615f82dcff3d2c
Krasimira Badova <krasi@hacksoft.io>
2018-02-11 17:01:05 +0200

    Open modal on click of picture

--- client/src/pages/Albums/Detail/index.jsx
+++ client/src/pages/Albums/Detail/index.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import _ from 'lodash';
 import {connect} from 'react-redux';
+
+import {Modal, ModalBody} from 'components/Modal';
+
 import {fetchAlbum} from './actions';
 import './styles.scss';
 
 class AlbumDetail extends React.Component {
+  state = {imageUrl: null};
+
   componentDidMount() {
     this.props.fetchAlbum(this.props.match.params.albumId);
   }
+
+  viewImage = imageUrl => {
+    console.log(imageUrl);
+    this.setState({imageUrl});
+  };
+
+  closeImage = () => this.setState({imageUrl: null});
 
   getAlbum = album => {
     return (
@@ -19,9 +31,15 @@ class AlbumDetail extends React.Component {
         {album.images &&
           album.images.map((img, index) => {
             return (
-              <div key={index}>
+              <div
+                key={index}
+                className="col-md-3"
+                onClick={() => this.viewImage(img.pathToFile)}>
                 name {img.name}
-                <img src={'http://localhost:3001/static/' + img.pathToFile} />
+                <img
+                  className="image"
+                  src={'http://localhost:3001/static/' + img.pathToFile}
+                />
               </div>
             );
           })}
@@ -30,10 +48,25 @@ class AlbumDetail extends React.Component {
   };
 
   render() {
+    const {imageUrl} = this.state;
     const {album} = this.props;
     return (
       <div className="album-container">
         {_.isNil(album) ? 'No album found' : this.getAlbum(album[0])}
+
+        {imageUrl && (
+          <Modal
+            show={true}
+            onCloseHandler={this.closeImage}
+            imageUrl={imageUrl}>
+            <ModalBody>
+              <div className="view-image">
+                <img src={'http://localhost:3001/static/' + imageUrl} />
+              </div>
+              <div>Tags and places here</div>
+            </ModalBody>
+          </Modal>
+        )}
       </div>
     );
   }
